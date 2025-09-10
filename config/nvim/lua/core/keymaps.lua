ea1bfb04ab51d0045970d4d055d64e54b3b2516a
Tritium55 <felixschuster2003@gmail.com>
2025-09-10 19:06:49 +0200

    adding keybinds for switching nvim windows more flaslessly

--- config/nvim/lua/core/keymaps.lua
+++ config/nvim/lua/core/keymaps.lua
@@ -15,3 +15,9 @@ vim.opt.expandtab = true
 
 -- some shortcuts
 vim.keymap.set('n', '<leader>h', vim.cmd.noh)
+
+-- better switching between windows
+vim.api.nvim_set_keymap('n', '<C-h>', '<C-w>h', { noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<C-j>', '<C-w>j', { noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<C-k>', '<C-w>k', { noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<C-l>', '<C-w>l', { noremap = true, silent = true})
