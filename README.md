2aaebf80b54d4da72d662c80c100da9d65a79ab9
Tritium55 <felixschuster2003@gmail.com>
2025-11-20 10:36:34 +0100

    LSP config fixed according to neovim v0.11+ standard. Still using Mason though, not the native LSP approach. (Using packer as well instead of native nvim pkg manager) Added description to README for updating keyboard layout on X11 systems.

--- README.md
+++ README.md
@@ -1,6 +1,24 @@
 # dotfiles
 My i3 .config files
 
+## Keyboard layout settings
+~~~bash
+# 1. Swap the physical Escape and Caps Lock keys entirely
+setxkbmap -option "caps:swapescape"
+
+# 2. Make the new Escape key (physical Caps Lock) act as Ctrl when held
+# The physical Caps Lock key is now internally an Escape key due to the swap above.
+# We use scape on the *resulting* Escape key, which does not naturally work.
+# So now, we set the physical Caps Lock key to behave as a Ctrl key.
+# Then we implement the Escape functionality.
+setxkbmap -option "caps:ctrl_modifier"
+
+# 3. Make the new Escape key (physically the Caps Lock key) send Escape when 
+xcape -e "Control_L=Escape"
+~~~
+You can those to e.g. `~/.xprofile`{:.bash}, `~/.xsession`{:.bash} or your desktop environment's autostart settings.
+Make sure said file is executable.
+
 ## Dependencies
 - i3
 - polybar
--- config/nvim/lua/core/plugin_config/lsp_config.lua
+++ config/nvim/lua/core/plugin_config/lsp_config.lua
@@ -1,5 +1,15 @@
-require("mason").setup()
-require("mason-lspconfig").setup( {
+require("mason").setup({
+    ui = {
+        icons = {
+            package_installed = "✓",
+            package_pending = "➜",
+            package_uninstalled = "✗"
+        }
+    }
+
+    -- log_level = vim.log.levels.DEBUG
+})
+require("mason-lspconfig").setup({
     -- ensure that all included language servers are installed
     ensure_installed = {
         "clangd",
@@ -27,16 +37,3 @@ local on_attach = function(_, _)
     vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
     -- vim.keymap.set('n', 'gn', vim.lsp.buf.next {})
 end
-
--- included language servers
-require("lspconfig").ast_grep.setup {}      -- C, Cpp, HTML, Java, Lua, Python, C#, Go, Rust, JS, TS, and CSS
-require("lspconfig").bashls.setup {}
-require("lspconfig").arduino_language_server.setup {}
-require("lspconfig").asm_lsp.setup {}
-require("lspconfig").clangd.setup {}
-require("lspconfig").cmake.setup {}     -- CMake
---require("lspconfig").ltex.setup {}      -- LaTeX and Markdown
-require("lspconfig").matlab_ls.setup {}
-require("lspconfig").lua_ls.setup {}
-require("lspconfig").pyright.setup {}
-
